520b3663049cc207daf3748c49cd2327218401a9
Mauricio Mutte <mauriciomutte.mm@gmail.com>
2022-12-09 16:56:14 -0300

    feat: add next config asset prefix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
 
-module.exports = nextConfig
+const isProd = process.env.NODE_ENV === 'production';
+const branchName = process.env.BRANCH_NAME;
+const assetPrefix = `${process.env.STATIC_CDN_URI}/${branchName}`;
+
+const nextConfig = {
+	reactStrictMode: true,
+	swcMinify: true,
+	assetPrefix: isProd ? assetPrefix : undefined,
+};
+
+module.exports = nextConfig;
