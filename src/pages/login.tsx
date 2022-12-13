821cfdcd590fafd08ef83b4e9929ac1a52a73aa3
Mauricio Mutte <mauriciomutte.mm@gmail.com>
2022-12-13 18:10:19 -0300

    fix: change home title to testing

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -13,9 +13,7 @@ const Home: NextPage = () => {
 			</Head>
 
 			<main className={styles.main}>
-				<h1 className={styles.title}>
-					Welcome to Pipeplayground (with Yarn 2)
-				</h1>
+				<h1 className={styles.title}>Welcome to Pipeplayground (with cache)</h1>
 			</main>
 		</div>
 	);
