d98c46b1ad6f9b1169f2674d352c03b87e3bcbac
Mauricio Mutte <mauriciomutte.mm@gmail.com>
2022-12-19 11:39:16 -0300

    feat: add assets URI by branch name

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
 
 const isProd = process.env.NODE_ENV === 'production';
+
 const branchName = process.env.BRANCH_NAME;
-const assetPrefix = `${process.env.STATIC_CDN_URI}/${branchName}`;
+
+const assetsURIByBranchName = {
+	main: `${process.env.STATIC_CDN_URI}/main`,
+	develop: `${process.env.STATIC_CDN_URI_DEV}/develop`,
+	qa: `${process.env.STATIC_CDN_URI_DEV}/qa`,
+};
+
+const assetPrefix = assetsURIByBranchName[branchName];
 
 const nextConfig = {
 	reactStrictMode: true,
