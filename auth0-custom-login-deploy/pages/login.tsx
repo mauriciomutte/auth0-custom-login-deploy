c636a47e9b4f33212967704ff6ad60530db31f2e
Mauricio Mutte <mauriciomutte.mm@gmail.com>
2022-12-28 13:58:18 -0300

    fix(auth0-example): remove unnecessary styles

---
+++ auth0-custom-login-deploy/pages/login.tsx
@@ -0,0 +1,32 @@
+import Head from 'next/head'
+import { Inter } from '@next/font/google'
+
+import styles from '../styles/Login.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Login() {
+  return (
+    <>
+      <Head>
+        <title>Auth0 custom login deploy</title>
+        <meta
+          name='description'
+          content='GenCreate a pipeline to automate Auth0 deploy'
+        />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <main className={styles.main}>
+        <div className={styles.center}>
+          <h1 className={inter.className}>Auth0 custom login deploy</h1>
+          <div className={styles.content}>
+            <p className={inter.className}>
+              Create a pipeline to automate Auth0 deploy
+            </p>
+          </div>
+        </div>
+      </main>
+    </>
+  )
+}
