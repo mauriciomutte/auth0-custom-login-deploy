f9fa293b366b0aab8f1311b6a4b9b1a86770fdd5
Mauricio Mutte <mauriciomutte.mm@gmail.com>
2022-12-28 14:08:39 -0300

    feat(auth0-example): add next configs

--- auth0-custom-login-deploy/next.config.js
+++ auth0-custom-login-deploy/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: isProd ? process.env.BASE_CDN_URL : undefined,
 }
 
 module.exports = nextConfig
